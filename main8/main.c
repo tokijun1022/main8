9cbf11465f9ed904b9d0dfae23df084635e42e72
TokimotoJun <juntokimoto@kagurdhcp024166.ph-in.kagu.tus.ac.jp>
2015-11-20 16:18:38 +0900

    Initial Commit

---
+++ main8/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  main8
+//
+//  Created by TokimotoJun on 2015/11/20.
+//  Copyright (c) 2015年 Jun Tokimoto. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
